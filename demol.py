be0927a3fd96b47e5fcbdb94acdde11f4d96afcb
Mengioio <87298556+Mengioio@users.noreply.github.com>
2021-07-13 16:23:07 +0800

    Add files via upload

---
+++ demol.py
@@ -0,0 +1,11 @@
+help("keywords")
+i=6
+print(i)
+print("hello world!")
+print("6")
+print(6+9)
+print(5*6)
+print(9/3)
+
+print("29号	  T血	        65.8	   632	       63")
+print("30号	   风衣	         96.8	   335	        78")
