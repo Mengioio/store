a39c615131aa4478113597afa9e75d449122d724
Mengioio <87298556+Mengioio@users.noreply.github.com>
2021-07-14 15:13:48 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,49 @@
+'''
+中文编码集：GBK
+iso：国际标准组织。
+    UTF-8 : SHIFT-JS  gbk  ASCII(万国码)
+
+变量 = 6
+
+print(变量)
+'''
+
+#
+a = 6
+b = 7
+# 写一段代码： + -
+
+a = a + b  # a = 13 b = 7
+b = a - b  # a = 13 b = 6
+a = a - b  # a = 7  b = 6
+
+print("a=",a,"b=",b)
+
+#
+A=56
+B=78
+
+A=A+B
+B=A-B
+A=A-B
+print("A=",A,"B=",B)
+
+#
+stu1=45
+stu2=23
+print(stu1+stu2)
+
+#
+a=50
+b=30
+c=40
+d=30
+e=40
+A=a+b+c+d+e
+B=round(A/5)
+print("A=",A,"B=",B)
+
+#
+num1=45
+num2=num1
+print("num2=",num2)
